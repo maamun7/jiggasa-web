aa25bc8e6fe31004cd173c5b74bca1a108affee1
Mamun Ahmed <maamun7@gmail.com>
2017-10-11 00:41:13 +0600

    Prepare sign-signup form in home modal page

--- dev/_components/home/homeModal.js
+++ dev/_components/home/homeModal.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
+import { Button, FormGroup, ControlLabel, FormControl, HelpBlock }
+from 'react-bootstrap';
 
 const customStyles = {
     overlay : {
@@ -13,7 +15,7 @@ const customStyles = {
     },
 
     content : {
-        top                        : '40%',
+        top                        : '50%',
         left                       : '50%',
         right                      : 'auto',
         bottom                     : 'auto',
@@ -25,7 +27,7 @@ const customStyles = {
         borderRadius               : '4px',
         outline                    : 'none',
         padding                    : '20px',
-        width                      : '35%',
+        width                      : '45%',
         height                     : '500px'
     }
 };
@@ -35,7 +37,9 @@ class HomeModal extends React.Component {
     constructor(props) {
         super(props);
 
-
+        this.state = {
+            value: ''
+        };
 
         this.openModal = this.openModal.bind(this);
         this.afterOpenModal = this.afterOpenModal.bind(this);
@@ -49,14 +53,26 @@ class HomeModal extends React.Component {
 
     afterOpenModal() {
         // references are now sync'd and can be accessed.
-        this.subtitle.style.color = '#f00';
+       // this.subtitle.style.color = '#f00';
     }
 
     closeModal() {
         this.setState({modalIsOpen: false});
     }
 
+    getValidationState() {
+        const length = this.state.value.length;
+        if (length > 10) return 'success';
+        else if (length > 5) return 'warning';
+        else if (length > 0) return 'error';
+    }
+
+    handleChange(e) {
+        this.setState({ value: e.target.value });
+    }
+
     render() {
+        const wellStyles = {maxWidth: '100%', margin: '0 auto 10px'};
 
         return (
             <div>
@@ -70,11 +86,57 @@ class HomeModal extends React.Component {
                     <center>
                         <img src={ require('../../../public/assets/img/logo.jpg') } height={55}/>
                     </center>
+                    <h2 ref={subtitle => this.subtitle = subtitle}> &nbsp; </h2>
 
-                   {/* <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
-                    <button onClick={this.props.onHide}>close</button>*/}
+                   {/* <button onClick={this.props.onHide}>close</button>*/}
 
+                    <div id="home-modal-container">
+                        <div className="row grid-divider">
+                            <div className="col-sm-6">
+                                <div className="col-padding">
+                                    <form>
+                                        <form>
+                                            <FormGroup controlId="formValidationSuccess1" validationState="success">
+                                                <FormControl type="text" placeholder="Name" />
+                                                {/*<HelpBlock>Help text with validation state.</HelpBlock>*/}
+                                            </FormGroup>
 
+                                            <FormGroup controlId="formValidationSuccess1" validationState="success">
+                                               {/* <ControlLabel>Input with error</ControlLabel>*/}
+                                                <FormControl type="text" placeholder="Email"  />
+                                            </FormGroup>
+
+                                            <FormGroup controlId="formValidationSuccess1" validationState="success">
+                                                <FormControl type="text" placeholder="Password" />
+                                                {/*<HelpBlock>Help text with validation state.</HelpBlock>*/}
+                                            </FormGroup>
+                                            <Button bsStyle="success" bsSize="primary" block>Signup</Button>
+
+                                        </form>
+                                    </form>
+                                </div>
+                            </div>
+                            <div className="col-sm-6">
+                                <div className="col-padding">
+                                    <form>
+                                         <ControlLabel> Signin </ControlLabel>
+                                        <FormGroup controlId="formValidationSuccess1" validationState="success">
+                                            {/* <ControlLabel>Input with error</ControlLabel>*/}
+                                            <FormControl type="text" placeholder="Email"  />
+                                        </FormGroup>
+
+                                        <FormGroup controlId="formValidationSuccess1" validationState="success">
+                                            <FormControl type="text" placeholder="Password" />
+                                            {/*<HelpBlock>Help text with validation state.</HelpBlock>*/}
+                                        </FormGroup>
+                                        <Button bsStyle="success" bsSize="primary" block>Signin</Button>
+                                        <Button bsStyle="link">Forgot password</Button>
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </Modal>
             </div>
         );
