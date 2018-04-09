eec3f968d3e0e4143da67b944827510dede64cb7
Mamun Ahmed <maamun7@gmail.com>
2018-04-09 18:35:06 +0600

    Solved  bootstarp js issues !

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import  'bootstrap';
 import './assets/styles/scss/style.scss';
 import  configureStore from './configStore';
 import RootContainer from './app/containers/App/';
