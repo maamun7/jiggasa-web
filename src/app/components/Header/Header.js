467230ce094a5e96f1f64b32790e0409a8410ac2
Mamun Ahmed <maamun7@gmail.com>
2018-04-10 19:46:03 +0600

    Working on header to reformat !

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { Component} from 'react';
 import { verifyAuthToken, getAuthInfo, doEmptyAuthSession } from '../../../utils/AuthHelper';
-import NavSearch from './NavSearch';
-import NavUser from './NavUser';
+import SessionHeader from './SessionHeader';
+import PlainHeader from './PlainHeader';
 
 const Header = ({
                     isAuthenticated,
@@ -18,21 +18,25 @@ const Header = ({
                         <div className="header-logo col-sm-2 col-md-2 col-lg-2">
                             <span className="logo-text">  জিজ্ঞাসা..? </span>
                         </div>
-                        <div className="header-search col-sm-7 col-md-7 col-lg-7">
-                            <NavSearch search={search}  />
-                        </div>
-                        <div className="header-button col-sm-3 col-md-3 col-lg-3 text-right">
-                            <button type="button" className="btn btn-sm btn-success btn-ask-question"> Ask New Question  </button>
-                            <NavUser
-                                isAuthenticated={isAuthenticated}
-                                user ={user}
-                                signUp ={signUp}
-                                signupResponse ={signupResponse}
-                                signupFail ={signupFail}
-                                signIn ={signIn}
-                                signinFail ={signinFail}
-                                signOut ={signOut}
-                            />
+                        <div className="col-sm-2 col-md-2 col-lg-2">
+                            if (isAuthenticated) {
+                                <SessionHeader            
+                                    isAuthenticated={isAuthenticated}
+                                    user ={user}
+                                    search ={search}
+                                    />
+                            } else {
+                                <PlainHeader   
+                                    isAuthenticated={isAuthenticated} 
+                                    signUp ={signUp}
+                                    signupResponse ={signupResponse}
+                                    signupFail ={signupFail}
+                                    signIn ={signIn}
+                                    signinFail ={signinFail}
+                                    signOut ={signOut} 
+                                    search ={search}
+                                    />
+                            }
                         </div>
                     </div>
                 );
