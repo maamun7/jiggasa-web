cb66fa4f9216cbec05a3d142e715eba30e20cda9
Mamun Ahmed <maamun7@gmail.com>
2018-04-10 19:46:23 +0600

    Working on header to reformat !

---
+++ src/app/components/Header/PlainHeader.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import AuthModal from '../Form/AuthModal';
+import SessionInfo from '../Header/SessionInfo';
+import NavSearch from './NavSearch';
+import NavUser from './NavUser';
+
+const PlainHeader = ({
+    isAuthenticated,
+    user,
+    signUp,
+    signupResponse,
+    signupFail,
+    signIn,
+    signinFail,
+    signOut,
+    search
+    }) => {
+        return (
+            <span>
+                <div className="header-search col-sm-7 col-md-7 col-lg-7">
+                    <NavSearch search={search}  />
+                </div>
+                <div className="header-button col-sm-3 col-md-3 col-lg-3 text-right">
+                    <button type="button" className="btn btn-sm btn-success btn-ask-question"> Ask New Question  </button>
+                    <AuthModal
+                        isAuthenticated={isAuthenticated}
+                        signUp ={signUp}
+                        signupResponse ={signupResponse}
+                        signupFail ={signupFail}
+                        signIn ={signIn}
+                        signinFail ={signinFail}
+                        />
+                </div>
+            </span>
+        );
+};
+
+export default PlainHeader;
